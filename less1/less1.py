1bb6f59bdcc7e38b7c27b2ea2e604bcbe4a52851
Vladimir <jidkov91@gmail.com>
2023-05-21 14:45:22 +0300

    first commit

---
+++ less1/less1.py
@@ -0,0 +1,10 @@
+# #вводится 2 числа 
+# a = int(input("Введите первое число "))
+# b = int(input("Введите второе число "))
+
+# if a > b:
+#     print("a > b")
+# elif a < b:
+#     print("a < b")
+# else:
+#     print("a = b")
