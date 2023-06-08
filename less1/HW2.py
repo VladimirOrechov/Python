33dbcb17e117cf4253c794c8f28eb9fd1c250842
Vladimir <jidkov91@gmail.com>
2023-06-08 15:50:28 +0300

    sdf

--- less1/HW2.py
+++ less1/HW2.py
@@ -43,8 +43,8 @@
 # 10 -> 1 2 4 8
 
 
-# n = int(input('Введите число: '))
-# i = 0
-# while 2 ** i <= n:
-#     print(2 ** i, end=' ')
-#     i +=1
+n = int(input('Введите число: '))
+i = 0
+while 2 ** i <= n:
+    print(2 ** i, end=' ')
+    i +=1
